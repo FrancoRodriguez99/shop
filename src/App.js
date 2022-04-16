2d85627c119f52c33a957833744914eb5cd168cd
FrancoRodriguez99 <juanfrancorodriguez99@gmail.com>
2022-04-16 01:53:56 -0300

    god damn it you git pages!

--- src/App.js
+++ src/App.js
@@ -3,58 +3,20 @@ import NavBar from "./components/NavBar/NavBar";
 import Presentation from "./components/Presentation/Presentation";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Counter from "./components/Counter/Counter";
-import Contacto from "./components/Contacto";
 import About from "./components/About";
-import Shop from "./components/Shop";
-import ShopNavBar from "./components/Shop/ShopNavBar";
 import Footer from "./components/Footer";
 import Services from "./components/Services";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Router className="App">
-      <div>
-        <NavBar />
-        <Presentation />
-        <Counter />
-        <About />
-        <Services />
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              <NavBar />
-              <Presentation />
-              <Counter />
-              <About />
-              <Services />
-            </div>
-          }
-        />
-        <Route
-          path="/Contacto"
-          element={
-            <div>
-              <NavBar />
-              <Contacto />
-            </div>
-          }
-        />
-        <Route
-          path="/Shop"
-          element={
-            <div>
-              <ShopNavBar />
-              <Shop />
-            </div>
-          }
-        />
-      </Routes>
+    <div>
+      <NavBar />
+      <Presentation />
+      <Counter />
+      <About />
+      <Services />
       <Footer />
-    </Router>
+    </div>
   );
 }
 
