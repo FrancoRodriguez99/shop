4262bd11805a8c8da71ceeb6cadcfce5c43355a7
FrancoRodriguez99 <juanfrancorodriguez99@gmail.com>
2022-04-20 11:00:47 -0300

    front end finished

--- src/components/Shop/ShopNavBar/index.js
+++ src/components/Shop/ShopNavBar/index.js
@@ -1,12 +1,17 @@
 import "./ShopNavBar.css";
 import CartShop from "../CartShop/";
 import image from "../../../assets/logoshop.png";
-
+import { search } from "../../../redux/slices/storeSlice";
 import shopcart from "../../../assets/shopcart.png";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function ShopNavBar() {
   const cart = useSelector((state) => state.cartshop);
+  const dispatch = useDispatch();
+  const displayvalue = (event) => {
+    const name = event.target.value;
+    dispatch(search(name));
+  };
   return (
     <div id="shopnavbarbox">
       <div id="shopnavbar">
@@ -14,7 +19,11 @@ function ShopNavBar() {
           <img alt="logo" src={image} id="logoshop"></img>
         </div>
         <div id="searchbarbox">
-          <input id="searchbar" placeholder="Buscar"></input>
+          <input
+            id="searchbar"
+            placeholder="Buscar"
+            onChange={(event) => displayvalue(event)}
+          ></input>
         </div>
         <div id="user">
           <button id="cartbtn">
--- src/redux/slices/storeSlice.js
+++ src/redux/slices/storeSlice.js
@@ -1,14 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 import producto from "../../constantes/items";
 
-let changeme = [...producto];
-
-let min = 0;
-let max = 99999999999999999999;
-
 export const storeSlice = createSlice({
   name: "store",
-  initialState: { storeitems: producto },
+  initialState: {
+    storeitems: [...producto],
+    constitems: [...producto],
+    min: 0,
+    max: 9999999999999,
+    estado: 0,
+    search: "",
+  },
   reducers: {
     getStore: (state, action) => {
       state.storeitems = action.payload;
@@ -19,79 +21,188 @@ export const storeSlice = createSlice({
           state.storeitems.sort(
             (a, b) => (a.precio[0] > b.precio[0] && 1) || -1
           );
-          changeme.sort((a, b) => (a.precio[0] > b.precio[0] && 1) || -1);
+          state.constitems.sort(
+            (a, b) => (a.precio[0] > b.precio[0] && 1) || -1
+          );
           break;
         case 1:
           state.storeitems.sort(
             (a, b) => (a.precio[0] < b.precio[0] && 1) || -1
           );
-          changeme.sort((a, b) => (a.precio[0] < b.precio[0] && 1) || -1);
+          state.constitems.sort(
+            (a, b) => (a.precio[0] < b.precio[0] && 1) || -1
+          );
           break;
         case 2:
           state.storeitems.sort(
             (a, b) => (a.cantidad[0] < b.cantidad[0] && 1) || -1
           );
-          changeme.sort((a, b) => (a.cantidad[0] < b.cantidad[0] && 1) || -1);
+          state.constitems.sort(
+            (a, b) => (a.cantidad[0] < b.cantidad[0] && 1) || -1
+          );
           break;
         case 3:
           state.storeitems.sort((a, b) => (a.id[0] < b.id[0] && 1) || -1);
-          changeme.sort((a, b) => (a.id[0] < b.id[0] && 1) || -1);
+          state.constitems.sort((a, b) => (a.id[0] < b.id[0] && 1) || -1);
+
           break;
         case 4:
-          state.storeitems = [...changeme];
+          state.storeitems = [...state.constitems];
+          state.estado = 1;
           state.storeitems = state.storeitems.filter((element) => {
             return element.estado;
           });
           state.storeitems = state.storeitems.filter((element) => {
             return (
-              element.precio[0] > min &&
-              element.precio[element.precio.length - 1] < max
+              element.precio[0] >= state.min &&
+              element.precio[element.precio.length - 1] <= state.max
             );
           });
+          if (state.search === "") {
+          } else {
+            state.storeitems = state.storeitems.filter((element) => {
+              return (
+                element.name
+                  .toLowerCase()
+                  .includes(state.search.toLowerCase()) ||
+                element.tags.toLowerCase().includes(state.search.toLowerCase())
+              );
+            });
+          }
           break;
         case 5:
-          state.storeitems = [...changeme];
+          state.storeitems = [...state.constitems];
+          state.estado = -1;
           state.storeitems = state.storeitems.filter((element) => {
             return !element.estado;
           });
           state.storeitems = state.storeitems.filter((element) => {
             return (
-              element.precio[0] > min &&
-              element.precio[element.precio.length - 1] < max
+              element.precio[0] >= state.min &&
+              element.precio[element.precio.length - 1] <= state.max
             );
           });
+          if (state.search === "") {
+          } else {
+            state.storeitems = state.storeitems.filter((element) => {
+              return (
+                element.name
+                  .toLowerCase()
+                  .includes(state.search.toLowerCase()) ||
+                element.tags.toLowerCase().includes(state.search.toLowerCase())
+              );
+            });
+          }
 
           break;
         case 6:
-          state.storeitems = [...changeme];
+          state.storeitems = [...state.constitems];
+          state.estado = 0;
+          state.storeitems = state.storeitems.filter((element) => {
+            return (
+              element.precio[0] >= state.min &&
+              element.precio[element.precio.length - 1] <= state.max
+            );
+          });
+          if (state.search === "") {
+          } else {
+            state.storeitems = state.storeitems.filter((element) => {
+              return (
+                element.name
+                  .toLowerCase()
+                  .includes(state.search.toLowerCase()) ||
+                element.tags.toLowerCase().includes(state.search.toLowerCase())
+              );
+            });
+          }
           break;
         default:
           break;
       }
     },
     filter: (state, action) => {
+      state.storeitems = [...state.constitems];
       if (action.payload[0] === 0) {
-        min = action.payload[1];
+        state.min = action.payload[1];
       } else {
-        max = action.payload[1];
+        state.max = action.payload[1];
       }
 
-      if (min === "") {
-        min = 0;
+      if (state.min === "") {
+        state.min = 0;
       }
-      if (max === "") {
-        max = 99999999999999999999;
+      if (state.max === "") {
+        state.max = 99999999999999999999;
       }
 
       state.storeitems = state.storeitems.filter((element) => {
         return (
-          element.precio[0] >= min &&
-          element.precio[element.precio.length - 1] <= max
+          element.precio[0] >= state.min &&
+          element.precio[element.precio.length - 1] <= state.max
         );
       });
+
+      switch (state.estado) {
+        case -1:
+          state.storeitems = state.storeitems.filter((element) => {
+            return !element.estado;
+          });
+          break;
+        case 1:
+          state.storeitems = state.storeitems.filter((element) => {
+            return element.estado;
+          });
+          break;
+        default:
+          break;
+      }
+
+      if (state.search === "") {
+      } else {
+        state.storeitems = state.storeitems.filter((element) => {
+          return (
+            element.name.toLowerCase().includes(state.search.toLowerCase()) ||
+            element.tags.toLowerCase().includes(state.search.toLowerCase())
+          );
+        });
+      }
+    },
+    search: (state, action) => {
+      state.storeitems = [...state.constitems];
+      state.search = action.payload;
+      if (state.search === "") {
+      } else {
+        state.storeitems = state.storeitems.filter((element) => {
+          return (
+            element.name.toLowerCase().includes(state.search.toLowerCase()) ||
+            element.tags.toLowerCase().includes(state.search.toLowerCase())
+          );
+        });
+        switch (state.estado) {
+          case -1:
+            state.storeitems = state.storeitems.filter((element) => {
+              return !element.estado;
+            });
+            break;
+          case 1:
+            state.storeitems = state.storeitems.filter((element) => {
+              return element.estado;
+            });
+            break;
+          default:
+            break;
+        }
+
+        state.storeitems = state.storeitems.filter((element) => {
+          return (
+            element.precio[0] >= state.min &&
+            element.precio[element.precio.length - 1] <= state.max
+          );
+        });
+      }
     },
   },
 });
 
-export const { getStore, sortStore, filter } = storeSlice.actions;
+export const { getStore, sortStore, filter, search } = storeSlice.actions;
 export default storeSlice.reducer;
